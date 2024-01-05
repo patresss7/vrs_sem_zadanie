9be2f93186fd3a5eb973fbaba133e38d781650e7
patresss7 <75572905+patresss7@users.noreply.github.com>
2024-01-05 11:16:04 +0100

    Setup pin for Button

--- KilometerTracker/Core/Src/gpio.c
+++ KilometerTracker/Core/Src/gpio.c
@@ -64,6 +64,12 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PB0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
   /*Configure GPIO pins : PB10 PB13 PB14 PB15
                            PB3 PB4 PB5 PB6 */
   GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15
